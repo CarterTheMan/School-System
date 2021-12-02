93f4245291f74937e232c747b8d60b701cc398ca
CarterTheMan <cartermoseley20@gmail.com>
2021-12-02 09:47:17 -0600

    Added some code for backend so when teachers make an assignment for a class, it adds it to all students in the class

--- Backend/src/main/java/myProject/CourseAssignmentController.java
+++ Backend/src/main/java/myProject/CourseAssignmentController.java
@@ -11,6 +11,15 @@ public class CourseAssignmentController {
 	@Autowired
 	CourseAssignmentRepository courseAssignment;
 	
+	@Autowired 
+	TeacherCourseRepository teacherCourse;
+	
+	@Autowired
+	StudentCourseRepository studentCourse;
+	
+	@Autowired 
+	StudentAssignmentRepository studentAssignment;
+	
 	//This allows the user to add a CourseAssignment
 	@RequestMapping(method = RequestMethod.POST, path = "/add-course-assignment")
 	CourseAssignment createCourseAssignment(@RequestBody CourseAssignment ca) {
@@ -28,5 +37,33 @@ public class CourseAssignmentController {
 	@RequestMapping(method = RequestMethod.GET, path = "/courseAssignment/{id}")
 	CourseAssignment getCourseAssignment(@PathVariable Integer id) {
 		return courseAssignment.findOne(id);
+	}
+	
+	
+	//SHOULD BE EITHER A POST OR A GET
+	//This creates a new assignment that trickles down to students
+	@RequestMapping(method = RequestMethod.POST, path = "/create-course-assignment/{tcid}/{title}/{description}")
+	CourseAssignment createCourseAssignmentTeacher(@RequestBody Integer tcid, @RequestBody String title, @RequestBody String description) {
+		//Create the new course assignment
+		TeacherCourse temp = teacherCourse.findOne(tcid);
+		CourseAssignment created = new CourseAssignment(temp, title, description);
+		courseAssignment.save(created);
+		
+		//Find all student courses for this class saved in students
+		List<StudentCourse> allSA = studentCourse.findAll();
+		List<StudentCourse> students = new ArrayList<StudentCourse>();
+		for (StudentCourse s : allSA) {
+			if (s.teacherCourse == temp) {
+				students.add(s);
+			}
+		}
+		
+		//Create new student assignments
+		for (int i = 0; i < students.size(); i++) {
+			StudentAssignment sa = new StudentAssignment(students.get(i), created, null, null);
+			studentAssignment.save(sa);
+		}
+		
+		return created;
 	}
 }
