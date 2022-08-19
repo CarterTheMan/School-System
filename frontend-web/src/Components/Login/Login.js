da69e7f60e54cc3cd2f77a34e856a60f4bc90215
CarterTheMan <cartermoseley20@gmail.com>
2022-08-19 03:25:38 -0500

    Made some progress on the login. It has to communicate with the server and it starts the fetch but runs into a problem with CORS so it doesn't work. Need to find some way around it which may involve adding a different backend function to act as a work around

--- frontend-web/src/Components/Login/Login.js
+++ frontend-web/src/Components/Login/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 class Login extends React.Component {
 
-    // Login: http://127.0.0.1:8080//login-student
+    // Login: http://127.0.0.1:8080/login-student       http://10.0.2.2:8080/login-student
     // To login you have to make a JSON object that gets passed to the above link
     // JSONObject object = new JSONObject();
     // try {
@@ -13,18 +13,25 @@ class Login extends React.Component {
     //     e.printStackTrace();
     // }
 
+    // To fix this issue, a new method for logging in may have to be made that takes the username and password as parameters
+    // ex: 127.0.0.1:8080/login-student/{username}/{password}
+
     // Link with example: https://www.codegrepper.com/code-examples/javascript/sending+json+to+url+react
-    // handleLogin2(username, password) {
-    //     // Simple POST request with a JSON body using fetch
-    //     const requestOptions = {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({ name: username, pass: password})
-    //     };
-    //     fetch('http://127.0.0.1:8080//login-student', requestOptions)
-    //         .then(response => response.json())
-    //         .then(data => this.setState({ postId: data.id }));      // This has to be changed
-    // }
+    handleLogin2() {
+        // Simple POST request with a JSON body using fetch
+        // console.log(document.getElementById('Username').value);
+        // var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+        const requestOptions = {
+            method: 'POST',
+            mode: 'cors',    // options: 'cors', 'no-cors', 'same-origin'
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "name": document.getElementById('Username').value, "password": document.getElementById('Password').value})
+        };
+        fetch('http://127.0.0.1:8080/login-student', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));      // This has to be changed to put the data somewhere
+        console.log("2");
+    }
 
     handleLogin() {
 
@@ -38,7 +45,7 @@ class Login extends React.Component {
                 <input type="text" id="Username" name="Username"></input>
                 <label for="Password">Password:</label>
                 <input type="text" id="Password" name="Password"></input>
-                <button onClick={this.handleLogin}>Login</button>
+                <button onClick={this.handleLogin2}>Login</button>
             </>
         )
     }
